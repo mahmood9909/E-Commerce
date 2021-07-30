5b7e3ee4215df01a119cd71ef34fdf06dc6d5978
Mahmood <Almahdi9909@gmail.com>
2021-07-31 01:53:30 +0300

    final editing for custom app bar

--- lib/widgets/dumb_widgts/app_bar/cutsom_app_bar.dart
+++ lib/widgets/dumb_widgts/app_bar/cutsom_app_bar.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/constant.dart';
 import 'package:ecommerce/screens/cart_page.dart';
 import 'package:ecommerce/service/db.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
